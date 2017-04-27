8c7b49e54430454a5d78a0beaf32605f6435e4ca
Lars Roith <LarsRoith@users.noreply.github.com>
2017-04-27 22:46:39 +0200

    Script for preparing build environment

---
+++ Scripts/CopyLibraries.ps1
@@ -0,0 +1,41 @@
+function CopyLibraries  {
+    param(
+        [Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+        [String] $vsExtensionsDirectory,
+        [Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+        [String] $copyDirectory
+    )
+    process{
+        $item = Get-ChildItem -Path "$vsExtensionsDirectory" -Include "Microsoft.VisualStudio.TeamFoundation.dll" -Recurse
+        Write-Host $item.FullName
+        if (Test-Path $item.FullName) {
+            Copy-Item -Path $item.FullName -Destination $copyDirectory
+        }
+        $item = Get-ChildItem -Path "$vsExtensionsDirectory" -Include "Microsoft.VisualStudio.TeamFoundation.VersionControl.dll" -Recurse
+        if (Test-Path $item.FullName) {
+            Copy-Item -Path $item.FullName -Destination $copyDirectory
+        }
+    }   
+}
+
+function GetVisualStudioExtensionsDirectory {
+    process {
+        return "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions"
+    }
+}
+
+function GetLibrariesDirectory {
+    process {
+        $path = [io.path]::combine($PSScriptRoot, '..', 'Lib\MS\Visual Studio\14.0')
+        if (-Not (Test-Path -PathType Container $path)) {
+            New-Item -ItemType Directory -Force -Path $path
+        }
+        return $path
+    }
+}
+
+$extensionDir = GetVisualStudioExtensionsDirectory
+$targetDir = GetLibrariesDirectory
+CopyLibraries $extensionDir $targetDir
